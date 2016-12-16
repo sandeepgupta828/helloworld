6da9865dfd430c8c88db742217f35ad4c4279f2b
Sandeep Gupta <sagupta@netflix.com>
2016-12-15 19:55:47 -0800

    Reset project

---
+++ src/com/programs/Main.java
@@ -0,0 +1,64 @@
+package com.programs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //MinAbsSum sol = new MinAbsSum();
+        //int[] arr = {2};
+        //int sum = sol.solution(arr);
+        //System.out.println(Integer.toString(sum));
+
+        //LCS lcs = new LCS();
+        //System.out.println(Integer.toString(lcs.solution("abcdx", "bcdefvx")));
+
+        //MaxProductSub max = new MaxProductSub();
+        //int[] subMax = max.solution(new int[] {1, 24, 0, 1, 1, -2, 0, 5,5});
+        //System.out.println(Arrays.toString(subMax));
+
+        //MinJumps minJumps = new MinJumps();
+        //int[] jumpArray = minJumps.solution(new int[] {1, 3, 5, 0, 1, 2, 1, 2, 1, 8, 9});
+        //System.out.println(Arrays.toString(jumpArray));
+
+        //TowersOfHanoi toh = new TowersOfHanoi();
+        //toh.solution(4);
+
+        //MedianOfSortedArrays mosa = new MedianOfSortedArrays();
+        //Integer median = mosa.solution(new int[] {1, 12, 15, 26, 38},new int[] {2, 13, 17, 30, 45});
+        //System.out.println("Median="+median);
+
+        //ComputeTreeDiameter ctd = new ComputeTreeDiameter();
+        //Node root = new Node();
+        //root.lst = new Node();
+        //root.rst = new Node();
+        //root.lst.lst = new Node();
+        //root.rst.rst = new Node();
+
+        //ctd.solution(root);
+
+        //ComputePercentile percentile = new ComputePercentile();
+        //percentile.setPercentile(50);
+        //percentile.addElements(new int[] {4,6,2,5,7,8,0,12,9,1});
+        //percentile.addElements(new int[] {41,61,21,51,71,81,11,12,91,66});
+
+        //LongestIncreasingSequence lis = new LongestIncreasingSequence();
+        //lis.solution(new int[] {10, 22, 9, 33, 21, 50, 41, 60, 80});
+
+        //AllPasswordsOnKeypad allPasswordsOnKeypad = new AllPasswordsOnKeypad();
+        //allPasswordsOnKeypad.solution(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
+        //allPasswordsOnKeypad.solution(null);
+
+        //AllWordsFromPhoneNumber allWordsFromPhoneNumber = new AllWordsFromPhoneNumber();
+        //allWordsFromPhoneNumber.solution(new int[] {4,6,6,5,2,2,4});
+
+        //Permute permute = new Permute();
+        //permute.solution("abcd");
+
+        Allcombinations allcombinations = new Allcombinations();
+        allcombinations.solution("abcdefgh");
+
+    }
+}
---
+++ src/com/programs/MinJumps.java
@@ -0,0 +1,39 @@
+package com.programs;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sagupta on 11/2/15.
+ */
+public class MinJumps {
+
+    public int[] solution(int[] inAr) {
+
+        int len = inAr.length;
+        int[] jumpArray = new int[inAr.length];
+        int k = 0;
+        jumpArray[k++] = inAr[0];
+        boolean jumpedToEnd = false;
+        for (int i=0;i<len-1 && !jumpedToEnd;) {
+            int maxReachSIndex = -1;
+            int maxReachTIndex = -1;
+            for (int j=i+1;j<=i+inAr[i] && j<len;j++) {
+                int reach = j + inAr[j];
+                if (reach >= (len-1)) {
+                    maxReachTIndex = len-1;
+                    maxReachSIndex = j;
+                    jumpedToEnd = true;
+                    break;
+                } else if (maxReachTIndex < reach) {
+                    maxReachTIndex = reach;
+                    maxReachSIndex = j;
+                }
+            }
+            if (maxReachTIndex > 0) {
+                i = maxReachSIndex;
+                jumpArray[k++] = inAr[maxReachSIndex];
+            }
+        }
+        return jumpArray;
+    }
+}
