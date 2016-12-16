5435359974cf3c9eecb3e6a8fa55c3425466c1a1
Sandeep Gupta <sagupta@netflix.com>
2016-12-15 19:56:46 -0800

    Reset project2

--- src/Node.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by sagupta on 11/3/15.
- */
-public class Node {
-    public Node lst;
-    public Node rst;
-    public Integer maxNodesToLeafOnLST;
-    public Integer maxNodesToLeafOnRST;
-}
