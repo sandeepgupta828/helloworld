bfe6658d7c33b4a4b9034e3c2154857f879af8a5
Sandeep Gupta <sagupta@netflix.com>
2018-08-29 17:47:58 -0700

    Anagrams

--- src/com/programs/Anagrams.java
+++ src/com/programs/Anagrams.java
@@ -1,52 +1,43 @@
 package com.programs;
 
-//public class Anagrams {
+import com.sun.xml.internal.fastinfoset.util.CharArray;
 
-/*
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-  */
-/*
- * To execute Java, please define "static void main" on a class
- * named Solution.
- *
- * If you need more classes, simply define them inline.
- */
 
-    //class Solution {
-/*
-        public static void main(String[] args) {
-            String[] input = {"dog", "cat", "atc", "god", "godess", "good"};
-            Solution sol = new Solution();
-            Map<String, List<String>> output = sol.findAnagrams(Arrays.asList(input));
-            //System.out.print(output);
-        }
+public class Anagrams {
 
-
-        public Map<String, List<String>> findAnagrams(List<String> wordList) {
-
-            Map<String, List<String>> map = new HashMap();
-
-            for (int i = 0; i < wordList.size(); i++) {
-                String word = wordList.get(i);
-
-                CharArray charArray = word.toCharArray();
-                String sortedWord = new String(Arrays.sort(charArray));
-
-                List<String> existingAnagrams = map.get(sortedWord);
-                if (existingAnagrams != null) {
-                    existingAnagrams.add(word);
-                } else {
-                    List<String> list = new ArrayList();
-                    list.add(word);
-                    map.put(sortedWord, list);
-                }
-            }
-            return map;
-        }
+    public static void main(String[] args) {
+        String[] input = {"dog", "cat", "atc", "god", "godess", "good"};
+        Anagrams anagrams = new Anagrams();
+        Map<String, List<String>> output = anagrams.findAnagrams(Arrays.asList(input));
+        System.out.println(output);
     }
-*/
+
+
+    public Map<String, List<String>> findAnagrams(List<String> wordList) {
+
+        Map<String, List<String>> map = new HashMap();
+
+        for (String word : wordList) {
+            char[] charArray = word.toCharArray();
+            Arrays.sort(charArray);
+            String sortedWord = new String(charArray);
+
+            List<String> existingAnagrams = map.get(sortedWord);
+            if (existingAnagrams != null) {
+                existingAnagrams.add(word);
+            } else {
+                List<String> list = new ArrayList();
+                list.add(word);
+                map.put(sortedWord, list);
+            }
+        }
+        return map;
+    }
+}
+
 
