c5c1d74d1ab1cc40f20675c36c5ea3604f98f203
Sandeep <sandeepgupta828@gmail.com>
2019-11-18 23:20:18 -0800

    Fix bugs

--- src/src/com/programs/mininterval/MinInterval.java
+++ src/src/com/programs/mininterval/MinInterval.java
@@ -1,7 +1,5 @@
 package com.programs.mininterval;
 
-import javafx.util.Pair;
-
 import java.util.*;
 
 public class MinInterval {
@@ -16,7 +14,7 @@ public class MinInterval {
         }
     }
 
-    public static Pair<Integer, Integer> findMinInterval(List<List<Integer>> listOfArrays) {
+    public static List<Integer> findMinInterval(List<List<Integer>> listOfArrays) {
         List<Entry> entryList = new ArrayList<>();
 
         // merge all the arrays integers into a single list, and map each integer to an entry obj having an integer and array id
@@ -36,14 +34,20 @@ public class MinInterval {
         int minEnd = -1;
 
         Map<String, Boolean> isArrPresent = new HashMap<>();
+        Map<String, Integer> lastIndex = new HashMap<>();
 
         for (int i = 0; i < entryList.size(); i++) {
             Entry entry = entryList.get(i);
+            lastIndex.put(entry.arId, i);
             Boolean wasPresent = isArrPresent.get(entry.arId);
             if (wasPresent == null || !wasPresent) {
                 isArrPresent.put(entry.arId, true);
             } else {
-                while (start < i && entryList.get(start).arId == entry.arId) start++;
+                // move the window forward if possible by incrementing start
+                // any entries at start that also have later last index <= i can be removed and windown advanced
+                // shortening the window leads to minimizing the distance between start and i, as entries are sorted
+                // the shortest distance is what we want across all window sizes
+                while (lastIndex.get(entryList.get(start).arId) > start) start++;
             }
             if (isArrPresent.size() == listOfArrays.size()) { // window is complete
                 // entries from all arrays are seen, now we have an interval where all arrays have entries i.e. [start, i]
@@ -55,17 +59,21 @@ public class MinInterval {
             }
         }
 
-        return new Pair<>(entryList.get(minStart).val, entryList.get(minEnd).val);
-
+        List<Integer> result = new ArrayList<>();
+        for (int i=0;i<entryList.size();i++) {
+            if (i >= minStart && i <= minEnd) {
+                result.add(entryList.get(i).val);
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         List<List<Integer>> listOfArrays = new ArrayList<>();
-        listOfArrays.add(Arrays.asList(new Integer[]{5, 7, 8, 19}));
-        listOfArrays.add(Arrays.asList(new Integer[]{6, 17, 18, 30}));
-        listOfArrays.add(Arrays.asList(new Integer[]{1, 9, 22}));
-        //listOfArrays.add(Arrays.asList(new Integer[]{9, 14, 20, 27}));
-
+        listOfArrays.add(Arrays.asList(new Integer[]{5, 7, 8}));
+        listOfArrays.add(Arrays.asList(new Integer[]{6, 17}));
+        listOfArrays.add(Arrays.asList(new Integer[]{1, 9}));
+        listOfArrays.add(Arrays.asList(new Integer[]{10, 14}));
         System.out.println(findMinInterval(listOfArrays));
     }
 }
