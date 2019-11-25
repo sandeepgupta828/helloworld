4bcbff7e06c7e9333233a7e18a2ece0599f68f21
Sandeep <sandeepgupta828@gmail.com>
2019-11-25 11:06:34 -0800

    Multiple questions (unsolved)

---
+++ src/src/com/programs/licenseplate/GenerateLicensePlates.java
@@ -0,0 +1,9 @@
+package com.programs.licenseplate;
+
+/**
+ * Generate all legit license 7 digit plate numbers that have 3 letters and 4 numbers and assign them to users for 10 years
+ * Some numbers expire and can be re-used
+ *
+ */
+public class GenerateLicensePlates {
+}
